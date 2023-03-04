3b894a787ee4b3a157c5fddc3ece63bbb996e2fd
Zerofrik <julianfe13@gmail.com>
2023-03-04 00:06:48 -0500

    modificacion del github
    empiezo la aplicacion

---
+++ App Julian Monroy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace App_Julian_Monroy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
